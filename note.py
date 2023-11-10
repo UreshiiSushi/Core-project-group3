5953286c59111d21339483b5b5cbab643d15a601
lomakina0612 <lomakina0612@gmail.com>
2023-11-10 12:29:01 +0100

    sort_tags

--- note.py
+++ note.py
@@ -1,19 +1,18 @@
 from collections import UserList
 
 
-class NoteBook:
-    def __init__(self):
-        self.notes = []
-
+class NoteBook(UserList):
     def add_note(self, text):
         tags = self.extract_tags(text)
-        self.notes.append({"text": text, "tags": tags})
+        self.data.append({"text": text, "tags": tags})
         print("Запись добавлена в блокнот.")
 
     def extract_tags(self, text):
-        # Реализуйте функцию для извлечения тегов из строки
         tags = [word[1:] for word in text.split() if word.startswith("#")]
         return tags
+
+    def sort_notes_by_tags(self):
+        self.data.sort(key=lambda note: len(note["tags"]))
 
     @staticmethod
     def add_note_from_user():
@@ -22,23 +21,23 @@ class NoteBook:
         notebook.add_note(input_text)
 
     def change_note(self, note_index, new_text):
-        if 0 <= note_index < len(self.notes):
+        if 0 <= note_index < len(self.data):
             tags = self.extract_tags(new_text)
-            self.notes[note_index] = {"text": new_text, "tags": tags}
+            self.data[note_index] = {"text": new_text, "tags": tags}
             print(f"Запись с индексом {note_index} изменена в блокноте.")
         else:
             print("Указанный индекс записи не существует.")
 
     def delete_note(self, note_index):
-        if 0 <= note_index < len(self.notes):
-            del self.notes[note_index]
+        if 0 <= note_index < len(self.data):
+            del self.data[note_index]
             print(f"Запись с индексом {note_index} удалена из блокнота.")
         else:
             print("Указанный индекс записи не существует.")
 
     def find_notes(self, search_text):
         matching_notes = []
-        for index, note in enumerate(self.notes):
+        for index, note in enumerate(self.data):
             if search_text.lower() in note["text"].lower():
                 matching_notes.append((index, note))
 
