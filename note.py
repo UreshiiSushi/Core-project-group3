d9dee23170835695654ccbe000bc884e263116a2
Jurij Procenko <elsoul@gmail.com>
2023-11-13 22:26:00 +0200

    refactored note.py

--- note.py
+++ note.py
@@ -15,16 +15,6 @@ class Notes:
         self.tag = tag
 
 
-class Notebook(UserList):
-    def __init__(self):
-        self.notes = []
-
-# class Notebook(UserList):
-#     def __init__(self):
-#         self.notes = []
-
-
-
 class NoteBook(UserList):
     def __init__(self):
         super().__init__()
@@ -34,104 +24,27 @@ class NoteBook(UserList):
         try:
             with open(save_file, "rb") as file:
                 self.data = pickle.load(file)
-                print("Заметки успешно загружены.")
+                print("Notebook succefully load")
         except FileNotFoundError:
-            print("Файл заметок не найден. Создан новый блокнот.")
+            print("Notes file not found. A new notepad has been created.")
         except Exception as e:
-            print(f"Ошибка при загрузке заметок: {e}")
+            print(f"Error loading notes: {e}")
 
     def save_notes(self):
         with open(save_file, "wb") as file:
             pickle.dump(self.data, file)
-            print("Заметки успешно сохранены.")
-
-    def note_main(self):
-        while True:
-            self.display_menu()
-            choice = input("Введите номер действия (help для справки): ")
-
-            if choice == "1":
-                self.add_note_from_user()
-            elif choice == "2":
-                self.display_all_notes()
-            elif choice == "3":
-                self.sort_notes_by_tags()
-                print("Записи отсортированы по тегам.")
-            elif choice == "4":
-                search_text = input("Введите текст для поиска: ")
-                self.find_notes(search_text)
-            elif choice == "5":
-                note_index = int(input("Введите индекс записи для изменения: "))
-                new_text = input("Введите новый текст: ")
-                self.change_note(note_index, new_text)
-            elif choice == "6":
-                note_index = int(input("Введите индекс записи для удаления: "))
-                self.delete_note(note_index)
-            elif choice == "help":
-                self.help()
-            elif choice == "0":
-                self.save_notes()
-                print("Выход из блокнота.")
-                break
-            else:
-                print("Некорректный ввод. Пожалуйста, выберите действие из меню.")
-
-    def display_menu(self):
-        print("\n===== Меню блокнота =====")
-        print("1. Добавить запись")
-        print("2. Вывести все записи")
-        print("3. Сортировать записи по тегам")
-        print("4. Найти записи по тексту")
-        print("5. Изменить запись")
-        print("6. Удалить запись")
-        print("0. Выйти из блокнота")
-        print("=========================\n")
-
-
-    def add_note_from_user(self):
-        input_text = input("Введите текст записи: ")
-        self.add_note(input_text)
+            print("Notes saved successfully")
 
     def add_note(self, text):
         tags = self.extract_tags(text)
         self.data.append({"text": text, "tags": tags})
-        print("Запись добавлена в блокнот.")
+        print("The note is added to the notepad")
 
     def display_all_notes(self):
-        print("\n===== Все записи в блокноте =====")
+        print("\n===== All notes from notebook =====")
         for index, note in enumerate(self.data):
-            print(f"Индекс: {index}, Текст: {note['text']}, Теги: {note['tags']}")
+            print(f"Note: {index}, Text: {note['text']}, Tags: {note['tags']}")
         print("==============================\n")
-
-    # def run_notebook(self):
-    #     while True:
-    #         self.display_menu()
-    #         choice = input("Введите номер действия (help для справки): ")
-
-    #         if choice == "1":
-    #             self.add_note_from_user()
-    #         elif choice == "2":
-    #             self.display_all_notes()
-    #         elif choice == "3":
-    #             self.sort_notes_by_tags()
-    #             print("Записи отсортированы по тегам.")
-    #         elif choice == "4":
-    #             search_text = input("Введите текст для поиска: ")
-    #             self.find_notes(search_text)
-    #         elif choice == "5":
-    #             note_index = int(input("Введите индекс записи для изменения: "))
-    #             new_text = input("Введите новый текст: ")
-    #             self.change_note(note_index, new_text)
-    #         elif choice == "6":
-    #             note_index = int(input("Введите индекс записи для удаления: "))
-    #             self.delete_note(note_index)
-    #         elif choice == "help":
-    #             self.help()
-    #         elif choice == "0":
-    #             print("Выход из блокнота.")
-    #             break
-    #         else:
-    #             print("Некорректный ввод. Пожалуйста, выберите действие из меню.")
 
     def extract_tags(self, text: str):
         tags = [word[1:] for word in text.split() if word.startswith("#")]
@@ -144,36 +57,29 @@ class NoteBook(UserList):
                 matching_notes.append((index, note))
 
         if matching_notes:
-            print("Найденные записи:")
+            print("Found records:")
             for index, note in matching_notes:
-                print(f"Индекс: {index}, Текст: {note['text']}, Теги: {note['tags']}")
+                print(f"Note: {index}, Text: {note['text']}, Tags: {note['tags']}")
         else:
-            print("Нет записей, соответствующих поисковому запросу.")
+            print("There are no records matching your search query")
 
     def sort_notes_by_tags(self):
         self.data.sort(key=lambda note: len(note["tags"]))
-
-    # @staticmethod
-    # def add_note_from_user():
-    #     notebook = NoteBook()
-    #     input_text = input("Введите текст записи: ")
-    #     notebook.add_note(input_text)
 
     def change_note(self, note_index, new_text):
         if 0 <= note_index < len(self.data):
             tags = self.extract_tags(new_text)
             self.data[note_index] = {"text": new_text, "tags": tags}
-            print(f"Запись с индексом {note_index} изменена в блокноте.")
+            print(f"Record with index {note_index} changed in notebook")
         else:
-            print("Указанный индекс записи не существует.")
+            print("The specified entry index does not exist")
 
     def delete_note(self, note_index):
         if 0 <= note_index < len(self.data):
             del self.data[note_index]
-            print(f"Запись с индексом {note_index} удалена из блокнота.")
+            print(f"Record with index {note_index} deleted in notenook")
         else:
-            print("Указанный индекс записи не существует.")
-
+            print("The specified entry index does not exist")
 
 
 notebook = NoteBook()
@@ -217,8 +123,6 @@ def help(_: list = None):
     print("==============================\n")
 
 
-# def get_user_input(prompt):
-#     return input(prompt)
 COMMANDS = {
     "add": add,
     "show": show,
@@ -240,74 +144,12 @@ def parser(text: str):
 
 def note_main():
     help()
-
     while True:
-        # menu = """
-        # Menu:
-        # 1. Add Note
-        # 2. Search note
-        # 3. Edit note
-        # 4. Delete Note
-        # 5. Sort Notes by tag
-        # 6. Exit
-        # """
-
-        # print(menu)
-
         choice = input("Enter your command >>> ")
-        if choice == "exit":
+        if choice.lower().startswith(("exit", "close", "quit")):
             notebook.save_notes()
             break
         parser(choice)
-
-        # if choice == "1":
-        #     title = input("Please enter title name: ")
-        #     description = input("Please enter description: ")
-        #     tag = input("Please enter tag: ")
-        #     notebook.add_note(title, description, tag)
-        #     print("Note has been added successfully!")
-
-        # elif choice == "2":
-        #     keyword = input("Enter key word for search: ")
-        #     found_notes = notebook.search_notes(keyword)
-        #     if found_notes:
-        #         print("Search results:")
-        #         for note in found_notes:
-        #             print(
-        #                 f"Title: {note.title}\nDescription: {note.description}\nTag: {note.tag}\n=========="
-        #             )
-        #     else:
-        #         print("Note is not found.")
-
-        # elif choice == "3":
-        #     note_index = int(input("Please enter note number you want to edit: "))
-        #     new_title = input("Enter a new title: ")
-        #     new_description = input("Enter a new description: ")
-        #     new_tag = input("Enter a new tag: ")
-        #     notebook.edit_note(note_index, new_title, new_description, new_tag)
-        #     print("Note has been edited successfully!")
-
-        # elif choice == "4":
-        #     note_index = int(input("Please enter note number you want to be removed: "))
-        #     notebook.delete_note(note_index)
-        #     print("Note has been removed successfully")
-
-        # elif choice == "5":
-        #     sorted_notes = notebook.sort_notes_by_tag()
-        #     if sorted_notes:
-        #         print("Sorted notes:")
-        #         for note in sorted_notes:
-        #             print(
-        #                 f"Title: {note.title}\nDescription: {note.description}\nTag: {note.tag}\n=========="
-        #             )
-        #     else:
-        #         print("No notes found.")
-
-        # elif choice == "6":
-        #     break
-
-        # else:
-        #     print("Wrong choice. Please try again.")
 
 
 if __name__ == "__main__":
